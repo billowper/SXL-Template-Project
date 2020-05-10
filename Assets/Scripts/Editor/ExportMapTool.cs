803b05ddd41746910b8122743def0d7c28f091d0
Bill Lowe <bill.lowe87@gmail.com>
2020-05-10 23:19:54 +0100

    Initial commit

---
+++ Assets/Scripts/Editor/ExportMapTool.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEditor;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// One-click export of the active scene to a unique asset bundle, with automatic version numbering
+/// Asset bundle is exporter to the project root folder and copied into the Skater XL maps folder in MyDocuments
+/// </summary>
+public static class ExportMapTool
+{
+    private const string ASSET_BUNDLES_BUILD_PATH = "AssetBundles";
+
+    [MenuItem("SXL/Export Map")]
+    public static void ExportMap()
+    {
+        var scene = SceneManager.GetActiveScene();
+
+        var version = EditorPrefs.GetInt($"{scene.name}_version", 1);
+
+        version++;
+
+        EditorPrefs.SetInt($"{scene.name}_version", version);
+
+        var build = new AssetBundleBuild
+        {
+            assetBundleName = $"{scene.name} v{version}",
+            assetNames = new[] {scene.path}
+        };
+
+        if (!Directory.Exists(ASSET_BUNDLES_BUILD_PATH))
+            Directory.CreateDirectory(ASSET_BUNDLES_BUILD_PATH);
+
+        BuildPipeline.BuildAssetBundles(ASSET_BUNDLES_BUILD_PATH, new []{ build }, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+
+        var docs_dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        var map_dir = Path.Combine(docs_dir, "SkaterXL/Maps");
+        var bundle_path = Path.Combine(Application.dataPath.Replace("/Assets", "/AssetBundles"), build.assetBundleName);
+        var dest_path = Path.Combine(map_dir, build.assetBundleName);
+      
+        Debug.Log($"Copying {bundle_path} to {dest_path}");
+
+        File.Copy(bundle_path, dest_path, overwrite: true);
+    }
+}
---
+++ Assets/Scripts/GrindPreview.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GrindPreview : MonoBehaviour
+{
+    public Color GizmoColor = Color.green;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = GizmoColor;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+
+            Gizmos.DrawSphere(child.transform.position, 0.05f);
+
+            if (i + 1 < transform.childCount)
+            {
+                Gizmos.DrawLine(child.transform.position, transform.GetChild(i + 1).position);
+            }
+        }
+    }
+}
