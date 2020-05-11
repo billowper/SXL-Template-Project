acd7d20fe82fe49e5b97a1897b107c9984adc471
Bill Lowe <bill.lowe87@gmail.com>
2020-05-11 23:26:00 +0100

    GrindSplineEditor

---
+++ Assets/Scripts/Editor/GrindSplineEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GrindSpline))]
+public class GrindSplineEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Add Point"))
+        {
+            var p = ((GrindSpline) target).transform;
+            var n = p.childCount;
+            var go = new GameObject($"Point {n + 1}");
+
+            go.transform.SetParent(p);
+            go.transform.localPosition = Vector3.zero;
+        }
+    }
+}
