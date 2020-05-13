e244c093fd7cbb474547fb835b5fdfca0c30a29c
Bill Lowe <bill.lowe87@gmail.com>
2020-05-14 00:38:23 +0100

    Prototype materials, GrindSpline collider generation

--- Assets/Scripts/Editor/GrindSplineEditor.cs
+++ Assets/Scripts/Editor/GrindSplineEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(GrindSpline))]
@@ -6,8 +7,6 @@ public class GrindSplineEditor : Editor
 {
     public override void OnInspectorGUI()
     {
-        base.OnInspectorGUI();
-
         if (GUILayout.Button("Add Point"))
         {
             var p = ((GrindSpline) target).transform;
@@ -16,6 +15,39 @@ public class GrindSplineEditor : Editor
 
             go.transform.SetParent(p);
             go.transform.localPosition = Vector3.zero;
+        }
+
+        serializedObject.UpdateIfRequiredOrScript();
+
+        EditorGUI.BeginChangeCheck();
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("GrindType"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("IsRound"));
+
+        if (serializedObject.FindProperty("IsRound").boolValue)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("GeneratedColliderRadius"));
+        }
+        else
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("GeneratedColliderWidth"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("GeneratedColliderDepth"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("IsEdge"));
+            
+            if (serializedObject.FindProperty("IsEdge").boolValue)
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("FlipEdgeSide"));
+        }
+
+        if (GUILayout.Button("Generate Colliders"))
+        {
+            ((GrindSpline) target).GenerateColliders();
+        }
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("GeneratedColliders"), true);
+        
+        if (EditorGUI.EndChangeCheck())
+        {
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
--- Assets/Scripts/GrindSpline.cs
+++ Assets/Scripts/GrindSpline.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -8,11 +9,16 @@ public class GrindSpline : MonoBehaviour
     {
         Concrete,
         Metal,
-        Wood
     }
 
     public Types GrindType;
     public bool IsRound;
+    public List<Collider> GeneratedColliders = new List<Collider>();
+    public float GeneratedColliderRadius = 0.1f;
+    public float GeneratedColliderWidth = 0.1f;
+    public float GeneratedColliderDepth = 0.05f;
+    public bool IsEdge;
+    public bool FlipEdgeSide;
 
     private Color gizmoColor = Color.green;
 
@@ -41,5 +47,67 @@ public class GrindSpline : MonoBehaviour
                 Gizmos.DrawLine(child.transform.position, transform.GetChild(i + 1).position);
             }
         }
+    }
+    
+    public void GenerateColliders()
+    {
+        foreach (var c in GeneratedColliders.ToArray())
+        {
+            if (c != null) 
+                DestroyImmediate(c.gameObject);
+        }
+        
+        GeneratedColliders.Clear();
+
+        for (int i = 0; i < transform.childCount - 1; i++)
+        {
+            var col = CreateColliderBetweenPoints(transform.GetChild(i).position, transform.GetChild(i + 1).position);
+
+            GeneratedColliders.Add(col);
+        }
+    }
+
+    private Collider CreateColliderBetweenPoints(Vector3 pointA, Vector3 pointB)
+    {
+        var go = new GameObject("Grind Cols")
+        {
+            layer = LayerMask.NameToLayer("Grindable")
+        };
+
+        go.transform.position = pointA;
+        go.transform.LookAt(pointB);
+        go.transform.SetParent(transform.parent);
+
+        switch (GrindType)
+        {
+            case Types.Concrete:
+                go.tag = "Grind_Concrete";
+                break;
+            case Types.Metal:
+                go.tag = "Grind_Metal";
+                break;
+        }
+
+        var length = Vector3.Distance(pointA, pointB);
+
+        if (IsRound)
+        {
+            var cap = go.AddComponent<CapsuleCollider>();
+
+            cap.direction = 2;
+            cap.radius = GeneratedColliderRadius;
+            cap.height = length + 2f * GeneratedColliderRadius;
+            cap.center = Vector3.forward * length / 2f + Vector3.down * GeneratedColliderRadius;
+        }
+        else
+        {
+            var box = go.AddComponent<BoxCollider>();
+
+            box.size = new Vector3(GeneratedColliderWidth, GeneratedColliderDepth, length);
+            var offset = IsEdge ? new Vector3(FlipEdgeSide ? (GeneratedColliderWidth / 2f) * -1 : GeneratedColliderWidth / 2f, 0, 0) : Vector3.zero;
+            box.center = offset + Vector3.forward * length / 2f + Vector3.down * GeneratedColliderDepth / 2f;
+        }
+        
+        return go.GetComponent<Collider>();
     }
 }
