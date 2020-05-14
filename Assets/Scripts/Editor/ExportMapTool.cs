a1f15bd9a418d408e84bff368208eac4a5accffa
Bill Lowe <bill.lowe87@gmail.com>
2020-05-14 20:57:59 +0100

    Grid mats, cleanup, added SplineMesh (free asset)

--- Assets/Scripts/Editor/ExportMapTool.cs
+++ Assets/Scripts/Editor/ExportMapTool.cs
@@ -16,8 +16,13 @@ public static class ExportMapTool
 {
     private const string ASSET_BUNDLES_BUILD_PATH = "AssetBundles";
 
-    [MenuItem("SXL/Export Map")]
+    [MenuItem("SXL/Quick Map Export")]
     public static void ExportMap()
+    {
+        ExportMap(null, EditorPrefs.GetBool("SXL_UseVersionNumbering", true));
+    }
+
+    public static void ExportMap(string override_asset_bundle_name, bool use_version_numbering)
     {
         var scene = SceneManager.GetActiveScene();
 
@@ -25,15 +30,27 @@ public static class ExportMapTool
 
         ProcessGrindsObjects(scene);
 
-        var version = EditorPrefs.GetInt($"{scene.name}_version", 1);
+        var bundle_name = scene.name;
 
-        version++;
+        if (use_version_numbering)
+        {
+            var version = EditorPrefs.GetInt($"{scene.name}_version", 1);
 
-        EditorPrefs.SetInt($"{scene.name}_version", version);
+            version++;
+
+            EditorPrefs.SetInt($"{scene.name}_version", version);
+
+            bundle_name = $"{scene.name} v{version}";
+        }
+
+        if (string.IsNullOrEmpty(override_asset_bundle_name) == false)
+        {
+            bundle_name = override_asset_bundle_name;
+        }
 
         var build = new AssetBundleBuild
         {
-            assetBundleName = $"{scene.name} v{version}",
+            assetBundleName = bundle_name,
             assetNames = new[] {scene.path}
         };
 
@@ -42,14 +59,14 @@ public static class ExportMapTool
 
         BuildPipeline.BuildAssetBundles(ASSET_BUNDLES_BUILD_PATH, new []{ build }, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
 
-        var docs_dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        var map_dir = Path.Combine(docs_dir, "SkaterXL/Maps");
+        var map_dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "SkaterXL/Maps");
         var bundle_path = Path.Combine(Application.dataPath.Replace("/Assets", "/AssetBundles"), build.assetBundleName);
         var dest_path = Path.Combine(map_dir, build.assetBundleName);
       
         Debug.Log($"Copying {bundle_path} to {dest_path}");
 
         File.Copy(bundle_path, dest_path, overwrite: true);
+        File.Delete(bundle_path);
 
         EditorSceneManager.OpenScene(scene.path);
     }
---
+++ Assets/Scripts/Editor/SXL_ToolsWindow.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SXL_ToolsWindow : EditorWindow
+{
+    private GUIStyle containerStyle;
+
+    [MenuItem("SXL/Tools Window")]
+    private static void Init()
+    {
+        GetWindow<SXL_ToolsWindow>().Show();
+    }
+
+    private void OnEnable()
+    {
+        containerStyle = new GUIStyle() {padding = new RectOffset(10, 10, 10, 10)};
+        UseVersionNumbering = EditorPrefs.GetBool("SXL_UseVersionNumbering", true);
+    }
+
+    private bool UseVersionNumbering;
+    private string OverrideAssetBundleName;
+
+    private void OnGUI()
+    {
+        EditorGUILayout.BeginVertical(containerStyle);
+        {
+            EditorGUI.BeginChangeCheck();
+
+            UseVersionNumbering = EditorGUILayout.Toggle(new GUIContent("Use Version Numbering", "If true, the exported AssetBundle will be appended with an incremental version number, e.g. Example Map v4"), UseVersionNumbering);
+            OverrideAssetBundleName = EditorGUILayout.TextField(new GUIContent("Override Name", "Optionally override the AssetBundle name (by default we just use the scene name)"), OverrideAssetBundleName);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                EditorPrefs.SetBool("SXL_UseVersionNumbering", UseVersionNumbering);
+            }
+
+            if (GUILayout.Button("Export Map"))
+            {
+                ExportMapTool.ExportMap(OverrideAssetBundleName, UseVersionNumbering);
+            }
+
+            if (GUILayout.Button("Delete Previous Versions"))
+            {
+                var scene = SceneManager.GetActiveScene();
+
+                if (EditorUtility.DisplayDialog("Are you sure?", $"This will delete all previously exported maps containing the name '{scene.name}'", "Yes", "Cancel"))
+                {
+                    var map_dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "SkaterXL/Maps");
+                    var paths = Directory.GetFiles(map_dir).Where(p => Path.GetFileName(p).StartsWith(scene.name)).ToArray();
+
+                    foreach (var p in paths)
+                    {
+                        Debug.Log($"Deleting '{p}'");
+                        File.Delete(p);
+                    }
+                } 
+            }
+        }
+        EditorGUILayout.EndVertical();
+    }
+}
