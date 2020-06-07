023b5e1c83ee3a9d28a24c658528e1b7c9e2f1c8
Bill <Lowe>
2020-06-07 21:51:54 +0100

    Split player scale ref placement tool

--- Assets/Scripts/Editor/GrindSplineGenerator.cs
+++ Assets/Scripts/Editor/GrindSplineGenerator.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
